af2cd772698a4919db13240633e0ea129ec34200
Jacob Baker <jsbaker73@gmail.com>
2015-11-21 13:40:51 -0800

    Added HealthSystem and HealthUI

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthUI : MonoBehaviour
+{
+	public HealthSystem HS;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
