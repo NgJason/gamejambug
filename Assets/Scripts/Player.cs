3325bef348435994a0bc6a3f3961a93b0d385ed0
Jason Ng <jason.ng@sjsu.edu>
2015-11-22 13:13:46 -0800

    Player facing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-
+	public Transform playerSprite;
     CharacterController c;
     public float moveSpeed = 1;
     private Vector3 pos;
@@ -18,7 +18,66 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+		Face();
+
     }
+
+	void Face(){
+
+
+
+
+
+		if (Input.GetAxis ("Horizontal") > 0 && Input.GetAxis ("Vertical") > 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, -135, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Horizontal") > 0 && Input.GetAxis ("Vertical") < 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, 135, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Horizontal") < 0 && Input.GetAxis ("Vertical") < 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, 45, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Horizontal") < 0 && Input.GetAxis ("Vertical") > 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, -45, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Horizontal") < 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, 90, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Horizontal") > 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, 270, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Vertical") > 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, 180, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}else if (Input.GetAxis ("Vertical") < 0) {
+			//faceing right
+			//playerSprite.eulerAngles = new Vector3(0,90,0);
+			//playerSprite.rotation = transform.right;
+			playerSprite.rotation = Quaternion.Euler(90, 0, 0);
+			//playerSprite.eulerAngles= transform.right;
+		}
+	}
 
     void Move()
     {
