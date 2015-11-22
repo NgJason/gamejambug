c7d851f208cb012c8704a7e951964124318220bb
Jacob Baker <jsbaker73@gmail.com>
2015-11-22 01:43:58 -0800

    added AI to the enemy

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,15 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class enemy : MonoBehaviour {
+public enum State{Pursuing, Returning, PatrollingA, PatrollingB};
 
-	// Use this for initialization
-	void Start () {
+public class enemy : MonoBehaviour
+{
+	public Transform	player;
+	public Transform	patrolA;
+	public Transform	patrolB;
+	public Vector3		startingPoint;
+	public State		currState;
+
+	public float	fieldOfView;
+	public float	visibilityDist;
+	public float	giveUpTime;
+
+	private NavMeshAgent	agent;
 	
+	float	timeLeft;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		startingPoint = transform.position;
+		agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if(SeesPlayer)
+		{
+			agent.SetDestination(player.position);
+			timeLeft = giveUpTime;
+			currState = State.Pursuing;
+		}
+
+		switch(currState)
+		{
+		case State.Pursuing:
+			if(timeLeft > 0)
+			{	timeLeft -= Time.deltaTime;	}
+			else
+			{
+				agent.SetDestination(startingPoint);
+				currState = State.Returning;
+			}
+			break;
+		case State.Returning:
+			if(Vector3.Distance(transform.position, startingPoint) < .1)
+			{	currState = State.PatrollingA;	}
+			break;
+		case State.PatrollingA:
+			if(Vector3.Distance(transform.position, patrolA) < .1)
+			{	currState = State.PatrollingB;	}
+			break;
+		case State.PatrollingB:
+			if(Vector3.Distance(transform.position, patrolB) < .1)
+			{	currState = State.PatrollingA;	}
+			break;
+		}
+	}
+
+	bool SeesPlayer()
+	{
+		RaycastHit hit;
+		Vector3 rayDirection = player.position - transform.position;
+
+		if(Vector3.Angle(rayDirection, transform.forward) <= fieldOfView / 2)
+		{
+			if (Physics.Raycast(transform.position, rayDirection, out hit, visibilityDist))
+			{	return (hit.transform.CompareTag("Player"));	}
+		}
+
+		return false;
 	}
 }
