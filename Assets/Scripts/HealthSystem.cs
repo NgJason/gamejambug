032d1e5c331b5c9684b55ca1f2c608e8be89016a
GSU <garysu413@gmail.com>
2015-11-22 12:18:46 -0800

    Finished HealthSystem

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,33 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HealthSystem : MonoBehaviour {
 
+
     public float curr_Health = 0f;
     public float max_Health = 100f;
-    public GameObject healthBar;
-
+    // Each healthBar is about 10%, so make ten for full health
+    public Slider healthBar;
 
 	// Use this for initialization
 	void Start () {
         curr_Health = max_Health;
+        
+        healthBar.value = curr_Health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+
 	}
 
-    public void DecreasingHealth()
+    public void DecreaseHealth(float damage)
     {
-        curr_Health -= 2f;
-        float calc_Health = curr_Health / max_Health;
-        SetHealthBar(calc_Health);
-    }
-
-    void SetHealthBar(float myHealth)
-    {
-        healthBar.transform.localScale = new Vector3(myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+        healthBar.value = healthBar.value - damage;
     }
 
 }
