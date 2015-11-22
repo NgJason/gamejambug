6d7695ce89a07305393d376e105b936c19c0eda6
GSU <garysu413@gmail.com>
2015-11-22 12:20:59 -0800

    Added Collision Damage

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -75,4 +75,12 @@ public class enemy : MonoBehaviour
 
 		return false;
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            player.SendMessage("DecreaseHealth", 10);
+        }
+    }
 }
